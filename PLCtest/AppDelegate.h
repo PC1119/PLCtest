6fa28d015c305199ebd5580940f0b815e214c41b
科技－18 易停 <yitingkeji-18@yitingkeji18demac-mini.local>
2016-01-11 10:42:05 +0800

    Initial Commit

---
+++ PLCtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PLCtest
+//
+//  Created by 易停科技－18 on 16/1/11.
+//  Copyright © 2016年 易停科技－18. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
